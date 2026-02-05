0146c5b68140af90f3f492d865edcd85fd535904
luongtrieudai2005 <52300185@student.tdtu.edu.vn>
2026-02-05 11:26:32 +0700

    Complete Bootloader

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,22 @@
-org 0x7C00
-bits 16
+[org 0x7C00]
+xor ax, ax		; Clear ax
+mov ds, ax		; Set data segment to 0
+mov es, ax		; Set extra segment to 0
+mov si, msg		; Set si to point to the message
+call print		; Call print function
+jmp $			; Infinite loop
 
-main:
-	hlt
+print:
+	lodsb		; Load character from si into al
+	or al, al	; Check for null terminator
+	jz done		; If null terminator, jump to done
+	mov ah, 0x0e	; Set teletype mode
+	int 0x10	; Call BIOS interupt to print character
+	jmp print	; Loop back to print next character
+done:
+	ret
+msg:
+	db "Welcome to my LuonOS", 0	; Null-terminated message string
 
-.halt:
-	jmp .halt
-
-times 510-($-$$) db 0
-dw 0AA55h
+times 510-($-$$) db 0; Fill the rest of the sector with 0s
+dw 0xaa55		; Boot signature
